12c64ae86503066ef8a2d65a3abe2798afe34ea2
Fernando Cardozo <carfer805@gmail.com>
2024-07-10 11:27:56 -0300

    cambios en el archivo 3.express de la clase 2

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,27 +1,43 @@
+const ditto = require('./pokemon/ditto.json');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT ?? 1234;
+app.disable('x-powered-by');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Mi pagina</h1>');
+app.use(express.json());
+// app.use((req, res, next) => {
+// if (req.method !== 'POST') return next();
+// if (req.headers['content-type'] !== 'application/json') return next();
+
+// let body = '';
+// escuchar el evento data
+// req.on('data', (chunk) => {
+// body += chunk.toString();
+// });
+
+// req.on('end', () => {
+// const data = JSON.parse(body); // convierte los datos binarios en string y los alamcena en data
+// data.timestamp = Date.now();
+// mutar la request y meter la informaion en el req.body
+// req.body = data;
+// next(); // va a intentar realizar la siguiete peticion
+// });
+// });
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto);
+});
+
+app.post('/pokemon', (req, res) => {
+  // req.body deberiamos guardar en Base de Datos
+  res.status(201).json(req.body);
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404</>');
 });
 
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`);
-});
-
-app.post('/pokemon', (req, res) => {
-  let body = '';
-
-  // escuchar el evento data
-  req.on('data', (chunk) => {
-    body += chunk.toString();
-  });
-
-  req.on('end', () => {
-    const data = JSON.parse(body);
-    data.timestamp = Date.now();
-    res.status(201).json(data);
-  });
 });
