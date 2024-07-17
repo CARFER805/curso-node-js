f4dcb1962bb83fdf67d1ad90fd08377fb79608a5
Fernando Cardozo <carfer805@gmail.com>
2024-07-17 00:42:49 -0300

    Creando mi primera app con NODEJS

---
+++ clase-3/app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const crypto = require('crypto'); // Ajuste en la importación de crypto
+const cors = require('cors');
+const app = express();
+const movies = require('./movies.json');
+const { validateMovie, validatePartialMovie } = require('./schemas/movie.js');
+
+app.use(express.json());
+app.use(cors());
+app.disable('x-powered-by'); //   el nombre del encabezado a deshabilitar
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'http://localhost://movies.com',
+];
+
+// Todos los recursos que sean MOVIES se identifican con /movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query;
+
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
+
+  res.json(movies);
+});
+
+app.get('/movies/:id', (req, res) => {
+  // path-to-regexp
+
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+  if (movie) {
+    return res.json(movie);
+  }
+  res.status(404).json({ message: 'Movie not found' });
+});
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body);
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid version 4
+    ...result.data,
+  };
+  //console.log(newMovie);
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
+});
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  console.log('entramos al DELETE');
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+  console.log('movieIndex: ', movieIndex);
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
+  movies.splice(movieIndex, 1);
+  return res.json({ message: 'Movie deleted' });
+});
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body);
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+  movies[movieIndex] = updateMovie;
+  return res.json(updateMovie);
+});
+
+const PORT = process.env.PORT ?? 1234;
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`);
+});
